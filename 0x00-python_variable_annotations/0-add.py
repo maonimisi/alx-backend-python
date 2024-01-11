dcab788bd8d4e2b423753b4d94cae54949dce68b
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-11 09:03:43 +0100

    Typed annotated function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes a float a float b as arguments
+and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """function add two float return sum as float"""
+    return a + b
