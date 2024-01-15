6617408126825ba5ff87279d3ffa97217d4fac89
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-15 11:05:54 +0100

    Python Async

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function (do not create an async function, use the regular function
+syntax to do this) task_wait_random that takes an integer max_delay and
+returns a asyncio.Task.
+"""
+import time
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int = 0) -> asyncio.Task:
+    """Function takes max_delay as an argument and return asyncio Task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
